1595d4dced5c1d287be1a6a8d3691b9c1dd43534
Heiner Pöpping <heiner.poepping@tobit.software>
2023-03-02 15:21:54 +0100

    :label: Fix type for useFunction

--- src/hooks/useFunction.ts
+++ src/hooks/useFunction.ts
@@ -2,7 +2,7 @@ import { useContextSelector } from 'use-context-selector';
 import { ChaynsFunctionsContext } from '../components/ChaynsContext';
 import { ChaynsReactFunctions } from '../types/IChaynsReact';
 
-export const useFunction = <T extends keyof ChaynsReactFunctions>(name): ChaynsReactFunctions[T] => {
-    const t = useContextSelector(ChaynsFunctionsContext, f => f ? f[name] : null)
+export const useFunction = <T extends keyof ChaynsReactFunctions>(name: T): ChaynsReactFunctions[T] => {
+    const t = useContextSelector(ChaynsFunctionsContext, f => f ? f[name] : null!)
     return t;
 }
