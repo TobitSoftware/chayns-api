60bb1b901b78cf063f3e66c50271795941b7f9d0
Heiner Pöpping <heiner.poepping@tobit.com>
2024-04-30 11:26:46 +0200

    :bug: Fix loadComponent

--- src/host/module/utils/loadComponent.ts
+++ src/host/module/utils/loadComponent.ts
@@ -61,34 +61,38 @@ const loadComponent = (scope, module, url, skipCompatMode = false, preventSingle
         console.warn('[chayns-api] skipCompatMode-option is deprecated and is set automatically now');
     }
 
-    if (!componentMap[scope] || !(module in componentMap[scope])) {
-        componentMap[scope][module] = React.lazy(() => {
-            return loadModule(scope, module, url, preventSingleton).then(async (Module: any) => {
-                if (typeof Module.default === 'function') {
-                    return Module;
-                }
-                const hostVersion = semver.minVersion(React.version)!;
-                const { requiredVersion, environment } = Module.default;
+    if (!componentMap[scope]) {
+        componentMap[scope] = {};
+    }
 
-                const shareScopes = await new Promise<Shared[]>(resolve => {
-                    loadShareSync('react', {
-                        resolver: (shareOptions) => {
-                            resolve(shareOptions);
-                            return shareOptions[0];
-                        },
-                    });
+    if (!(module in componentMap[scope])) {
+        const promise = loadModule(scope, module, url, preventSingleton).then(async (Module: any) => {
+            if (typeof Module.default === 'function') {
+                return Module;
+            }
+            const hostVersion = semver.minVersion(React.version)!;
+            const { requiredVersion, environment } = Module.default;
+
+            const shareScopes = await new Promise<Shared[]>(resolve => {
+                loadShareSync('react', {
+                    resolver: (shareOptions) => {
+                        resolve(shareOptions);
+                        return shareOptions[0];
+                    },
                 });
-                const matchReactVersion = requiredVersion && semver.satisfies(hostVersion, requiredVersion) && !shareScopes.some((t) => {
-                    const {version, from } = t;
-                    return (semver.gt(version, hostVersion) && semver.satisfies(version, requiredVersion)) || scope === from.split('-').join('_');
-                })
-
-                if (!matchReactVersion || environment !== 'production' || process.env.NODE_ENV === 'development') {
-                    return { default: Module.default.CompatComponent };
-                }
-                return { default: Module.default.Component };
             });
-        })
+            const matchReactVersion = requiredVersion && semver.satisfies(hostVersion, requiredVersion) && !shareScopes.some((t) => {
+                const {version, from } = t;
+                return (semver.gt(version, hostVersion) && semver.satisfies(version, requiredVersion)) || scope === from.split('-').join('_');
+            })
+
+            if (!matchReactVersion || environment !== 'production' || process.env.NODE_ENV === 'development') {
+                return { default: Module.default.CompatComponent };
+            }
+            return { default: Module.default.Component };
+        });
+
+        componentMap[scope][module] = React.lazy(() => promise);
     }
     return componentMap[scope][module];
 }
