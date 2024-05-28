5fdd467d4cf0969f55e38283d62354589d3e7cea
Marius <marius.gertdenken@tobit.software>
2024-05-28 14:29:45 +0200

    Use compat component for old modules

--- src/host/module/utils/loadComponent.ts
+++ src/host/module/utils/loadComponent.ts
@@ -89,7 +89,7 @@ const loadComponent = (scope, module, url, skipCompatMode = false, preventSingle
                 return (semver.gt(version, hostVersion) && semver.satisfies(version, requiredVersion)) || scope === from.split('-').join('_');
             })
 
-            if (!matchReactVersion || environment !== 'production' || process.env.NODE_ENV === 'development') {
+            if (Module.default.version !== 2 || !matchReactVersion || environment !== 'production' || process.env.NODE_ENV === 'development') {
                 return { default: Module.default.CompatComponent };
             }
             return { default: Module.default.Component };
