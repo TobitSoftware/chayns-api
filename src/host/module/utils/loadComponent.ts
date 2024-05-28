453ece51fb338183699171d6bc16f3389ba9641b
Marius <marius.gertdenken@tobit.software>
2024-05-28 14:22:59 +0200

    Try change share scope

--- src/host/module/utils/loadComponent.ts
+++ src/host/module/utils/loadComponent.ts
@@ -14,23 +14,18 @@ init({
     shared: {
         react: {
             version: React.version,
-            scope: 'default',
+            scope: 'defaultV2',
             lib: () => React,
         },
         'react-dom': {
             version: ReactDOM.version,
-            scope: 'default',
+            scope: 'defaultV2',
             lib: () => ReactDOM,
         },
     },
 });
 
 export const loadModule = async (scope, module, url, preventSingleton = false) => {
-    // @ts-expect-error
-    if (typeof __webpack_init_sharing__ === 'function') {
-        // @ts-expect-error
-        await __webpack_init_sharing__('default');
-    }
     if (registeredScopes[scope] !== url || preventSingleton) {
         if (scope in registeredScopes) {
             console.error(`[chayns-api] call registerRemote with force for scope ${scope}. url: ${url}`);
@@ -82,18 +77,6 @@ const loadComponent = (scope, module, url, skipCompatMode = false, preventSingle
             const { requiredVersion, environment } = Module.default;
 
             const shareScopes = await new Promise<{ version: string, from: string }[]>(resolve => {
-                // @ts-expect-error
-                if (typeof __webpack_share_scopes__ !== 'undefined') {
-                    // @ts-expect-error
-                    const shareScopes = __webpack_share_scopes__.default;
-                    console.log('__webpack_share_scopes__', shareScopes);
-                    const shareList = Object.entries<object & { from: string }>(shareScopes.react).map(([k, v]) => ({
-                        version: k,
-                        from: v.from,
-                    }));
-                    resolve(shareList);
-                }
-
                 loadShareSync('react', {
                     resolver: (shareOptions) => {
                         resolve(shareOptions);
