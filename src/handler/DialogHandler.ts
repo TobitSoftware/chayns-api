4fc8d8413cd1fcb4f75989708c2481e38ea8546f
Marius <marius.gertdenken@tobit.software>
2024-07-03 16:20:52 +0200

    Remove test values

--- src/handler/DialogHandler.ts
+++ src/handler/DialogHandler.ts
@@ -17,7 +17,7 @@ export default class DialogHandler<S = void, T extends any = void> {
         this._addDataListener = addDataListener;
     }
 
-    async open(): Promise<{ buttonType: number, value: T extends undefined ? S : T, s: S }> {
+    async open(): Promise<{ buttonType: number, value: T extends undefined ? S : T }> {
         if (this.isOpen) {
             throw new Error('cannot open a dialog which is already open');
         }
@@ -36,7 +36,7 @@ export default class DialogHandler<S = void, T extends any = void> {
                 this.listeners.forEach((cb) => cb(data));
             });
         });
-        return this.result = res as { buttonType: number, value: T extends undefined ? S : T, s: S }
+        return this.result = res as { buttonType: number, value: T extends undefined ? S : T }
     }
 
     close(buttonType, data) {
