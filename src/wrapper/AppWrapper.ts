e403100cabdc55aae081bc371c80e5f8c08270ee
Jannik Weise <jannik.weise@tobit.com>
2024-07-31 09:52:35 +0200

    🐛 Fix reference error on site data update after design settings changed

--- src/wrapper/AppWrapper.ts
+++ src/wrapper/AppWrapper.ts
@@ -562,8 +562,8 @@ export class AppWrapper implements IChaynsReact {
             enabled: true,
         }, {
             callback: ({ colorMode }) => {
-                this.values.site.colorMode = colorMode;
-                document.dispatchEvent(new CustomEvent('chayns_api_data', { detail: { type: 'site', value: this.values.site } }));
+                const site = { ...this.values.site, colorMode };
+                document.dispatchEvent(new CustomEvent('chayns_api_data', { detail: { type: 'site', value: site } }));
             },
             awaitResult: true
         });
