a16c877ba44842658af5337f92e236d138a34bf7
Marius <marius.gertdenken@tobit.software>
2024-10-23 11:57:59 +0200

    Change init

--- src/util/initModuleFederationSharing.ts
+++ src/util/initModuleFederationSharing.ts
@@ -1,35 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-export const initModuleFederationSharing = () => {
+export const initModuleFederationSharing = ({ name }) => {
     // forces single instance of module federation runtime
-    if (!globalThis.moduleFederationRuntime) {
-        globalThis.moduleFederationRuntime = require('@module-federation/enhanced/runtime');
-        globalThis.moduleFederationScopes = {
-            registeredScopes: {},
-            moduleMap: {},
-            componentMap: {},
-        }
-
-        const { init } = globalThis.moduleFederationRuntime;
-
-        // init also should only be called once
-        init({
-            // will be set by chayns-toolkit via DefinePlugin
-            name: process.env.__PACKAGE_NAME__ ?? '',
-            remotes: [],
-            shared: {
-                react: {
-                    version: React.version,
-                    scope: 'chayns-api',
-                    lib: () => React,
-                },
-                'react-dom': {
-                    version: ReactDOM.version,
-                    scope: 'chayns-api',
-                    lib: () => ReactDOM,
-                },
-            },
-        });
+    if(globalThis.moduleFederationRuntime) {
+        return;
     }
+
+    globalThis.moduleFederationRuntime = require('@module-federation/enhanced/runtime');
+    globalThis.moduleFederationScopes = {
+        registeredScopes: {},
+        moduleMap: {},
+        componentMap: {},
+    }
+
+    const { init } = globalThis.moduleFederationRuntime;
+
+    init({
+        name: name ?? '',
+        remotes: [],
+        shared: {
+            react: {
+                version: React.version,
+                scope: 'chayns-api',
+                lib: () => React,
+            },
+            'react-dom': {
+                version: React.version, // intended, because react dom.version is not identical to package json react version (hash in version)
+                scope: 'chayns-api',
+                lib: () => ReactDOM,
+            },
+        },
+    });
 }
