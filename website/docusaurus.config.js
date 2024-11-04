c4aa763d15a1afce611fbbbedfc2145eac723410
Heiner Pöpping <heiner.poepping@tobit.com>
2024-11-04 18:33:35 +0100

    :construction: WIP update docusaurus

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = {
 	title: "chayns-api",
 	tagline: "",
-	url: "https://tobitsoftware.github.io/chayns-api",
+	url: "https://tobitsoftware.github.io",
 	baseUrl: "/chayns-api/",
 	onBrokenLinks: "warn",
 	onBrokenMarkdownLinks: "warn",
@@ -14,10 +14,10 @@ module.exports = {
 	themeConfig: {
 		colorMode: {
 			respectPrefersColorScheme: true,
-			switchConfig: {
-				darkIcon: "🌑",
-				lightIcon: "💡",
-			},
+			// switchConfig: {
+			// 	darkIcon: "🌑",
+			// 	lightIcon: "💡",
+			// },
 		},
 		navbar: {
             title: 'chayns-api',
