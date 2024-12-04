abe4e99adffb45fb415ab10c930582717c30a69e
Heiner Pöpping <heiner.poepping@tobit.com>
2024-12-04 19:21:16 +0100

    Prevent creation of new empty user object

--- src/hooks/useUser.tsx
+++ src/hooks/useUser.tsx
@@ -2,7 +2,9 @@ import { useContextSelector } from 'use-context-selector';
 import { ChaynsContext } from '../components/ChaynsContext';
 import { ChaynsApiUser } from '../types/IChaynsReact';
 
+
+const empty = {}
 /**
  * @category Hooks
  */
-export const useUser = (): ChaynsApiUser => useContextSelector(ChaynsContext, v => v?.user || {})
+export const useUser = (): ChaynsApiUser => useContextSelector(ChaynsContext, v => v?.user || empty)
