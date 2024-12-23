e63de7730025682ebe0b7165c665c604c5e3418f
Heiner Pöpping <heiner.poepping@tobit.com>
2024-12-23 10:46:43 +0100

    Add react-dom/client to shared when possible

--- src/util/initModuleFederationSharing.ts
+++ src/util/initModuleFederationSharing.ts
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+let ReactDOMClient;
+try {
+    ReactDOMClient = require('react-dom/client');
+} catch (e) {
+    // do nothing
+}
 
 export const initModuleFederationSharing = ({ name }) => {
     // forces single instance of module federation runtime
@@ -16,20 +22,29 @@ export const initModuleFederationSharing = ({ name }) => {
 
     const { init } = globalThis.moduleFederationRuntime;
 
+    const shared = {
+        react: {
+            version: React.version,
+            scope: 'chayns-api',
+            lib: () => React,
+        },
+        'react-dom': {
+            version: React.version, // intended, because react dom.version is not identical to package json react version (hash in version)
+            scope: 'chayns-api',
+            lib: () => ReactDOM,
+        },
+    };
+    if (ReactDOMClient) {
+        shared['react-dom/client'] = {
+            version: React.version,
+            scope: 'chayns-api',
+            lib: () => ReactDOMClient
+        }
+    }
+
     init({
         name: name ?? '',
         remotes: [],
-        shared: {
-            react: {
-                version: React.version,
-                scope: 'chayns-api',
-                lib: () => React,
-            },
-            'react-dom': {
-                version: React.version, // intended, because react dom.version is not identical to package json react version (hash in version)
-                scope: 'chayns-api',
-                lib: () => ReactDOM,
-            },
-        },
+        shared,
     });
 }
