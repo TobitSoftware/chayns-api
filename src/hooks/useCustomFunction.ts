13fc2cdc1421b8b24ce788dc79a3c0161826d18f
Heiner Pöpping <heiner.poepping@tobit.com>
2025-01-07 18:49:01 +0100

    :label: Change type for custom function

--- src/hooks/useCustomFunction.ts
+++ src/hooks/useCustomFunction.ts
@@ -1,3 +1,3 @@
 import { useCustomFunctionsSelector } from './context';
 
-export const useCustomFunction = (key: string) => useCustomFunctionsSelector((customFunctions) => customFunctions[key]);
+export const useCustomFunction = <A extends Array<any>, T>(key: string) => useCustomFunctionsSelector<(...args: A) => Promise<T>>((customFunctions) => customFunctions[key]);
