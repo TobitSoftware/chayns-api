68b039f0b1aeb483350613b86a32cc754c703816
Heiner Pöpping <heiner.poepping@tobit.software>
2025-08-14 23:34:33 +0200

    :art: Use stack to fix singleton issues
    Also fixes memory leak during SSR or when using renderToString

--- src/components/ChaynsProvider.tsx
+++ src/components/ChaynsProvider.tsx
@@ -7,7 +7,7 @@ import { FrameWrapper } from '../wrapper/FrameWrapper';
 import { ModuleFederationWrapper } from '../wrapper/ModuleFederationWrapper';
 import { SsrWrapper } from '../wrapper/SsrWrapper';
 import { ChaynsContext } from './ChaynsContext';
-import { moduleWrapper, moduleWrapperList } from './moduleWrapper';
+import { addModuleWrapper, moduleWrapper, removeModuleWrapper } from './moduleWrapper';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 
@@ -29,7 +29,6 @@ type ChaynsProviderProps = {
     renderedByServer?: boolean,
     isModule?: boolean,
     children?: ReactNode,
-    preventOverrideSingleton?: boolean,
 }
 
 const ChaynsProvider: React.FC<ChaynsProviderProps> = ({
@@ -39,7 +38,6 @@ const ChaynsProvider: React.FC<ChaynsProviderProps> = ({
     customFunctions,
     renderedByServer,
     isModule,
-    preventOverrideSingleton,
 }) => {
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     const customWrapper = useRef<IChaynsReact>(null!);
@@ -66,10 +64,7 @@ const ChaynsProvider: React.FC<ChaynsProviderProps> = ({
                 customWrapper.current = new FrameWrapper();
             }
         }
-        moduleWrapperList.push(customWrapper.current);
-        if (!preventOverrideSingleton || !moduleWrapper.current) {
-            moduleWrapper.current = customWrapper.current;
-        }
+        moduleWrapper.current = customWrapper.current;
     }
 
     const [isInitialized, setIsInitialized] = useState<boolean>(!!customWrapper.current?.values);
@@ -100,14 +95,11 @@ const ChaynsProvider: React.FC<ChaynsProviderProps> = ({
         }
     }, [customFunctions, isModule]);
 
-    useEffect(() => () => {
-        const index = moduleWrapperList.indexOf(customWrapper.current);
-        if (index > -1) {
-            moduleWrapperList.splice(index, 1);
-        }
-        if (moduleWrapper.current === customWrapper.current) {
-            moduleWrapper.current = moduleWrapperList[0];
-        }
+    useEffect(() => {
+        addModuleWrapper(customWrapper.current);
+        return () => {
+            removeModuleWrapper(customWrapper.current);
+        };
     }, []);
 
     return (
--- src/components/moduleWrapper.ts
+++ src/components/moduleWrapper.ts
@@ -1,4 +1,27 @@
 import { IChaynsReact } from '../types/IChaynsReact';
 
-export const moduleWrapperList: IChaynsReact[] = [];
-export const moduleWrapper: { current: IChaynsReact } = { current: undefined! }
+const moduleWrapperStack: IChaynsReact[] = [];
+let current: IChaynsReact = undefined!;
+
+export const moduleWrapper: { current: IChaynsReact } = {
+    get current(): IChaynsReact {
+        if (moduleWrapperStack.length === 0 && !current) {
+            throw new Error("No chayns api instance intialized");
+        }
+        return moduleWrapperStack.at(-1) || current;
+    },
+    set current(chayns: IChaynsReact) {
+        current = chayns;
+    }
+ }
+
+export const addModuleWrapper = (chayns: IChaynsReact) => {
+    moduleWrapperStack.push(chayns);
+}
+
+ export const removeModuleWrapper = (chayns: IChaynsReact) => {
+    const index = moduleWrapperStack.indexOf(chayns);
+    if (index > -1) {
+        moduleWrapperStack.splice(index, 1);
+    }
+ }
