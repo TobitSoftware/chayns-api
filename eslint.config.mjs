83a027229d045870dbd0563e6f44d4c599c899c2
Heiner Pöpping <heiner.poepping@tobit.com>
2026-05-08 15:08:48 +0200

    :sparkles: Add React Compiler support

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,9 @@
 import config from '@chayns-toolkit/eslint-config';
 
+import hooksPlugin from 'eslint-plugin-react-hooks';
+
+// Enable rules for react-compiler which are not enabled by default
+const rule = config.find(rule => rule.plugins?.['react-hooks']);
+Object.assign(rule.rules, hooksPlugin.configs['recommended-latest'][0].rules);
+
 export default config;
